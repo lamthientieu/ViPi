613bf09b27a7ce55d7f1aac6e1df1ecd4ec1955d
lamthientieu <57694952+lamthientieu@users.noreply.github.com>
2022-07-11 23:23:59 +0700

    Update start.py

--- src/start.py
+++ src/start.py
@@ -9,9 +9,6 @@ print("\ncheck system: ",a)
 try:      
     with open('{}/.config/google-oauthlib-tool/credentials.json'.format(USER_PATH), 'r') as registers:
         register = json.load(registers)
-
-    with open('{}/.config/google-oauthlib-tool/credentials.json'.format(USER_PATH), 'r') as registers:
-        register = json.load(registers)
     if configuration['Wakewords']['Ok_Google']=='Disabled' and 'armv7l' not in platform.platform():
         print ('\nimport main6')
         import main6
@@ -21,11 +18,11 @@ try:
         import main7
         main7.Myassistant().main()
     if 'armv7l' in platform.platform() and configuration['Wakewords']['Ok_Google']=='Enabled':
-        import main
         print ('\nimport main')
+        import main
         main.Myassistant().main()
     else:
-        say_save('gặp lỗi không khởi động được, vui lòng kiểm tra cài đặt')
+        print('gặp lỗi không khởi động được, vui lòng kiểm tra cài đặt')
         pass
 except:  
     print('Bạn chưa đăng ký tài khoản với google, thực hiện đăng ký tài khoản')
